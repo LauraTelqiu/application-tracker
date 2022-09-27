44df34309b2d5a7aaadef8492fb0061248615680
Laura <lauratelqiu@gmail.com>
2022-09-27 13:27:36 -0400

    commit

--- src/components/ApplicationItem.js
+++ src/components/ApplicationItem.js
@@ -1,15 +1,18 @@
 import React from "react";
-import "./styles/ApplicationItem.css"
+import ApplicationDate from "./ApplicationDate";
+import "./styles/ApplicationItem.css";
 
 function ApplicationItem(props) {
-
   return (
-    <div>
-      <div>{props.date.toISOString()}</div>
-      <div>
-        <div>{props.companyName}</div>
-        <div>{props.status}</div>
-        <div>{props.notes}</div>
+    <div className="application-item">
+      <ApplicationDate date={props.date} />
+      <div className="application-item__description">
+        <h2>{`Company Name: ${props.companyName}`}</h2>
+        <h3>{`notes: ${props.notes}`}</h3>
+        <div className="application-item__notes">
+          <h2>{props.status}</h2>
+
+        </div>
       </div>
     </div>
   );
